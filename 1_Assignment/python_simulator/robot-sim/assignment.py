675f995bcd4eb07bd294ac2b0b5d957b4c3fe7af
Andrea Chiappe <chiappe.emiliani99@gmail.com>
2023-11-12 10:13:30 +0100

    correct code

--- 1_Assignment/python_simulator/robot-sim/assignment.py
+++ 1_Assignment/python_simulator/robot-sim/assignment.py
@@ -22,6 +22,7 @@ marker_found = []
 marker_release = set()
 """set of all the marker release near the firt box"""
 
+total_markers = set()
 
 #####################################################################################
 
@@ -76,7 +77,6 @@ def find_token():
     
 ############################################################################################### 
 
-
 def find_first_token():
     """
     Function to find the closest token
@@ -97,9 +97,8 @@ def find_first_token():
         return dist, rot_y, token
     
 ############################################################################################### 
-
   
-def drive_to_token(dist, rot_y):
+def drive_to_token(dist, rot_y, d_thr):
     """
     Function that drive the robot to the object
     Input:
@@ -116,7 +115,7 @@ def drive_to_token(dist, rot_y):
         return 1          
     elif -a_th <= rot_y <= a_th:  # if the robot is well aligned with the token, we go forward
         print("Ah, here we are!.")
-        drive(80, 0.1)
+        drive(60, 0.1)
     #Control the rotation of the robot respect to marker
     elif rot_y < -a_th:  
         print("Left a bit...")
@@ -127,15 +126,16 @@ def drive_to_token(dist, rot_y):
         
 #################################################################################################################
 
-def drive_to_first_token(dist, rot_y):   
+def drive_to_first_token(dist, rot_y, d_thr):   
+    #In this function the only change resoect 
 
     
-    if dist < 0.5:
+    if dist < d_thr:
         print("Found it!")    
         return 1          
     elif -a_th <= rot_y <= a_th:  # if the robot is well aligned with the token, we go forward
         print("Ah, here we are!.")
-        drive(30, 0.1)
+        drive(60, 0.1)
     #Control the rotation of the robot respect to marker
     elif rot_y < -a_th:  
         print("Left a bit...")
@@ -145,127 +145,115 @@ def drive_to_first_token(dist, rot_y):
         turn(+2, 0.1)
                             
 ######################################################################################################################
-
-
-
            
 def main():
     """The main function has to find the first marker, and then carry to him all the marker presents in arena"""
     """ Consider out of while the case of first token found"""     
+    var = 1
+    count = 0
+    while(var):
+        turn(20, 0.5)
+        marker = R.see()
+        for m in marker:
+            total_markers.add(m.info.code)  
+        print(total_markers) 
+        count += 1
+        if count == 15:
+            var = 0
+    
     grab = False
-    var  = 1 # the variable for first while lopp useful for find the first marker
-    while 1:   
-        
-        #find the nearest marker, it will be the first markers and will carry all token near him
-        while(var):
-            
-            if find_token() == -1: #in this case I turn until the robot can't see any token
-                print("I can't see any token!!")
-                turn(20, 0.5) 
-            else:
-                print("drive to the first nearest token")
-                dist_1, rot_y_1, token = find_token() 
-                if drive_to_token(dist_1, rot_y_1) ==  1:
-                    print("add the token in the merker_found list")
-                    
-                    marker_found.append(token.info.code) #put the code of the marker inside the list of founded marker                    
-                    grab = R.grab()
-                    print("found marker: ")
-                    print(marker_found)
-                                    
-                    R.release()
-                    print("add the marker in the marker_Release set")
-                    marker_release.add(token.info.code) #add the marker in the 
-                    print("now the release marker are: ")
-                    print(marker_release)
-                    
-                    grab = False  #set to false the grab value, because I dont have any token in hand
-                    
-                    drive(-20, 1) #drive back for avoid to hit the marker 
-                    turn(-40, 0.5) #turn on right because I want to carry now the marker on the right 
-                
-                    var = 0     # close the while loop 
-                
-        #Now i look for the other marker
-        if find_token() == -1: #control that the robot can see the object
-            print("OUTSIDE IF")
-            print("I can't see any token!!")
-            turn(-20, 0.5)           
+    while 1: 
+        if(len(total_markers) == 0):
+            print("work done")
+            exit()
         else:
-            dist, rot_y, token = find_token() # we look for the nearest marker
+            #Now i look for the other marker
+            if find_token() == -1: #control that the robot can see the object
+                #print("OUTSIDE IF")
+                print("I can't see any token!!")
+                turn(-20, 0.5)           
+            else:
+                dist, rot_y, token = find_token() # we look for the nearest marker
+                print("near token")
+                print(token.info.code)  
+                print("release set")
+                print(marker_release)
+                print("found set")
+                print(marker_found)  
+                print("total markers")
+                print(total_markers)                  
             
-            print("outside the for cycle")
-            print("near token")
-            print(token.info.code)  
-            print("release set")
-            print(marker_release)
-            print("found set")
-            print(marker_found)                    
-        
-            ###########################################################################################
-            #if the marker is release jet and not grab, entre here if the token is in the correct area
-            
-            if token.info.code in marker_release and not(grab):
-                print("FIRST IF")
-                print("I have already pose this token, need to find an other token")
-                turn(-20, 0.5) #turn because I have founded that marker jet
-            
-            ############################################################################################
-            #if the marker is in the hand of the robot (grab == True)
-            
-            elif grab: 
-            #I take him near the first marker                               
-                print("SECOND ELIF")
+                ###########################################################################################
+                #if the marker is release jet and not grab, entre here if the token is in the correct area
                 
-                if find_first_token() == -1: #in this case I turn until the robot can't see any token
-                    print("SECOND ELIF")
-                    print("I can't see any token!!")
-                    turn(-20, 0.5) 
-                else:
-                    dist, rot_y, token = find_first_token() 
-                    # Check all the markers the robot can see
-                    print(token.info.code)
+                if token.info.code in marker_release and not(grab):
+                    #print("FIRST IF")
+                    print("I have already pose this token, need to find an other token")
+                    turn(-20, 0.5) #turn because I have founded that marker jet
+                
+                ############################################################################################
+                #if the marker is in the hand of the robot (grab == True)
+                
+                elif grab: 
+                #I take him near the first marker                               
+                    #rint("SECOND ELIF")
                     
-                    if token.info.code in marker_release: 
-                        print("SECOND IF -> if")                        
-                        print(token.info.code)
-                        if drive_to_first_token(dist, rot_y) == 1:
-                            print("arrived to first token")
-                            print("release the marker amd add to release set")
-                            R.release()
-                            #add the last element of the marker_found setin marker_release set
-                            marker_release.add(marker_found[-1]) 
-                            grab = False #set the grab value to False, now the robot doesn't have any token in his arm
-                            print(grab)
-                            drive(-20, 1)
+                    if find_first_token() == -1: #in this case I turn until the robot can't see any token
+                        #print("SECOND ELIF")
+                        print("I can't see any token!!")
+                        turn(-20, 0.5) 
                     else:
-                        turn(-20, 0.5)
-                                           
-            #################################################################################################
-            #control if I never release before this object and if the hand of robot are free
-            
-            elif token.info.code not in marker_release and not(grab) :
-                print("THIRD ELIF")
-                print(token.info.code)
-                if drive_to_token(dist, rot_y) == 1:
-                    marker_found.append(token.info.code) #Put the marker code inside the list of founded marker                   
-                    print("found another marker: ")
-                    print(marker_found)  
-                    print(marker_release)
-                    grab = R.grab() #set to true the grab value
+                        dist, rot_y, token = find_first_token() 
+                        # Check all the markers the robot can see
+                        print(token.info.code)
                         
+                        if token.info.code in marker_release: 
+                            print("SECOND IF -> if")                        
+                            print(token.info.code)
+                            if drive_to_first_token(dist, rot_y, 0.5) == 1:
+                                print("arrived to first token")
+                                print("release the marker amd add to release set")
+                                R.release()
+                                
+                                total_markers.pop() #remove one casua element in this set
+                                marker_release.add(marker_found[-1]) #add the last element of the marker_found setin marker_release set
+                                grab = False #set the grab value to False, now the robot doesn't have any token in his arm
+                                print(grab)
+                                drive(-20, 1)
+                        else:
+                            turn(-20, 0.5)
+                                            
+                #################################################################################################
+                #control if I never release before this object and if the hand of robot are free
+                
+                elif token.info.code not in marker_release and not(grab) :
+                    #print("THIRD ELIF")
+                    #print(token.info.code)
+                    
+                    if len(marker_release) == 0:
+                        #print("THIRD IF -> if")
+                    #case when I haven't find any token
+                        print("drive to the first nearest token")
+                        if drive_to_token(dist, rot_y, d_th) ==  1:                            
+                            marker_found.append(token.info.code) #put the code of the marker inside the list of founded marker                    
+                            grab = R.grab() #set ture the grab value
+                            R.release()
+                            total_markers.pop() #remove one element in the total_mark set
+                            print("add the marker in the marker_Release set")                            
+                            marker_release.add(token.info.code) #add the marker in the marker_rrelease se                          
+                            grab = False  #set to false the grab value, because I dont have any token in hand
+                            
+                            drive(-20, 1) #drive back for avoid to hit the marker                            
                         
-                        
-                      
-              
+                    else:
+                        #print("THIRD IF ->else")
+                        if drive_to_token(dist, rot_y, d_th) == 1:
+                            marker_found.append(token.info.code) #Put the marker code inside the list of founded marker                   
+                            grab = R.grab() #set to true the grab value
+                                    
+                 
                          
-      
 
 main()
-
-
-
-
-
 
 
